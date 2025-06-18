c3d6d43212ed06c43b1d84eb3de9d8c0d87f6876
Jorge <jsalas@thepineschool.org>
2025-06-18 19:29:13 -0400

    Cars and Motorcycles

--- ch3/cars.py
+++ ch3/cars.py
@@ -7,3 +7,32 @@ print(cars)
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 cars.sort(reverse=True)
 print(cars)
+
+print("---------") # seperator
+
+# Sorting a List Temporarily with the sorted() Function
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
+
+print("---------") # seperator
+
+# Printing a List in Reverse Order
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+cars.reverse()
+print(cars)
+
+print("---------") # seperator
+
+# Finding the Length of a List
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))
---
+++ ch3/motorcycles_errors.py
@@ -0,0 +1,7 @@
+# Avoiding Index Errors When Working with Lists
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles[3])
+print(motorcycles[-1])
+
+motorcycles = []
+# print(motorcycles[-1])
